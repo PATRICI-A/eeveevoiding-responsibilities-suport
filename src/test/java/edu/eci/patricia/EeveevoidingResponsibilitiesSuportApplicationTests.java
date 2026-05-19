5e65f7a849a81be94956c67293262ef50f6f81db
CamiloWGod4564 <juan.cristancho-v@mail.escuelaing.edu.co>
2026-05-19 11:46:31 -0500

    recursos test

--- src/test/java/edu/eci/patricia/EeveevoidingResponsibilitiesSuportApplicationTests.java
+++ src/test/java/edu/eci/patricia/EeveevoidingResponsibilitiesSuportApplicationTests.java
@@ -4,8 +4,10 @@ package edu.eci.patricia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EeveevoidingResponsibilitiesSuportApplicationTests {
 
     @Test
