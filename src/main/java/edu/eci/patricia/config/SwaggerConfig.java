46988d6d3051e570ae7ad07ee1eb268ca0bf2ccd
CamiloWGod4564 <juan.cristancho-v@mail.escuelaing.edu.co>
2026-05-23 01:21:31 -0500

    cambio swagger

--- src/main/java/edu/eci/patricia/config/SwaggerConfig.java
+++ src/main/java/edu/eci/patricia/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package edu.eci.patricia.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -49,11 +48,7 @@ public class SwaggerConfig {
                                 All write endpoints require a valid Bearer JWT token issued by the authentication \
                                 service. The user ID is extracted from the JWT `sub` claim.
                                 """)
-                        .version("v1.0.0")
-                        .contact(new Contact()
-                                .name("PATRICI.A Platform Team")
-                                .email("soporte.bienestar@eci.edu.co")
-                                .url("https://www.escuelaing.edu.co")))
+                        .version("v1.0.0"))
                 .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
                 .components(new Components()
                         .addSecuritySchemes(BEARER_SCHEME, new SecurityScheme()
